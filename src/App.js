8e0acbadda34139d0ad8cb4ac0ec2711a08a0be0
saji37 <sajiththomas162631@gmail.com>
2022-11-15 15:43:48 +0530

    Fetching data and using useQuery

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React,{useState} from 'react'
+import './App.css'
+import { useQuery } from 'react-query'
+import Post from './Post'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	// let's go
+const fetcher= url => fetch(url).then(res => res.json())
+const [state,setState]= useState(null);
+
+const {isLoading, data:posts} = useQuery(['posts'],() => fetcher('https://jsonplaceholder.typicode.com/posts'))
+
+const goBack = ()=>{
+    setState(null);
 }
 
-export default App;
+if(isLoading)
+return <h1>Loading....</h1>
+
+const searchOne = (id) =>{
+    setState(id)
+}
+if(state!= null)
+    return <p><Post id={state} goback={goBack} /></p>
+
+	return (
+		<div className="App">
+			<h2>Blog posts</h2>
+            {posts.map((post)=>{
+                return <a href="#" onClick={()=>searchOne(post.id)}><p >{post.title}</p></a>
+            })}
+		</div>
+	)
+}
+
+export default App
